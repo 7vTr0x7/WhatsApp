8da6fc7162f2eadc6f9253505d610d174e2aa243
7vTr0x7 <7vtrox7@gmail.com>
2024-04-21 14:51:39 +0530

    Convex schema

---
+++ convex/schema.ts
@@ -0,0 +1,13 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  task: defineTable({
+    text: v.string(),
+    completed: v.boolean(),
+  }),
+  products: defineTable({
+    name: v.string(),
+    price: v.number(),
+  }),
+});
